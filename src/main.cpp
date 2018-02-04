b885b9ea225e75e65a85fce636b5dd645ee32133
Lukas Jurk <ljurk@l2k3.ddns.net>
2018-02-04 12:50:37 +0100

    changed cc order to fit unsorted pots in the controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 
 int cc = 176; //controlChange on Channel 1
-byte controlNum[8] = {21,22,23,24,25,26,27,28};
+byte controlNum[8] = {102,104,100,106,101,105,103,107};
 //pots
 int potVal[8];
+int potValOld[8];
 byte controlVal[8];
-byte controlValOld[8];
+//byte controlValOld[8];
 //iterator
 byte i = 0;
 //tolarance for pot Movement
 byte tolarance = 5;
+
 void sendCC(byte cc,byte controlNum,byte value) {
   Serial.write(cc);
   Serial.write(controlNum);
@@ -36,14 +38,14 @@ void loop () {
     potVal[i] = 0.2 * potVal[i] + 0.8 * analogRead(A0);
     controlVal[i] = map(potVal[i],0,1023,0,127);
 
-    if(controlVal[i] <= controlValOld[i] - tolarance || controlVal[i] > controlValOld[i] + tolarance ) {
+    if(potVal[i] <= potValOld[i] - tolarance || potVal[i] > potValOld[i] + tolarance ) {
       delay(200);
       sendCC(cc,controlNum[i], controlVal[i]);
       //Serial.print(i);
       //Serial.print(":");
       //Serial.print(controlVal[i]);
       //Serial.println("");
-      controlValOld[i] = controlVal[i];
+      potValOld[i] = potVal[i];
     }
   }
 }
